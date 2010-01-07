af2eaff89936e7b11e427e194e00d9877609449e
pui <pui@a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a>
2010-01-07 09:52:58 +0000

    git-svn-id: https://opserver:443/svn/pojjaman/trunk@142 a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a

---
+++ CopyDLL_IF_Error.bat
@@ -0,0 +1,10 @@
+copy "Pojjaman.ContentInterface\bin\Pojjaman.ContentInterface.dll" "Pojjaman\bin"
+copy "Longkong.Reporting\bin\Longkong.Reporting.dll" "Pojjaman\bin"
+copy "Longkong.BasePropertyControl\bin\Longkong.BasePropertyControl.dll" "Pojjaman\bin"
+copy "Pojjaman.Data\bin\Pojjaman.Data.dll" "Pojjaman\bin
+copy "Pojjaman.CustomControls\bin\Pojjaman.CustomControls.dll" "Pojjaman\bin"
+copy "Pojjaman.CustomControls\bin\Longkong.CustomControls.dll" "Pojjaman\bin"
+
+
+"Pojjaman\bin\Pojjaman.exe" 
+
