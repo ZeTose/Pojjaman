264ba1e6fdd0087d02f45eeb04faad9e71c9ca25
teeraboon <teeraboon@a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a>
2010-06-16 07:57:59 +0000

    แก้กลับ อันตะกี้ ส่งเกิน
    git-svn-id: https://opserver:443/svn/pojjaman/trunk@712 a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a

--- Pojjaman.BusinessLogic/BusinessEntities/Check/Outgoing/UpdateCheckPayment.vb
+++ Pojjaman.BusinessLogic/BusinessEntities/Check/Outgoing/UpdateCheckPayment.vb
@@ -844,11 +844,11 @@ Namespace Longkong.Pojjaman.BusinessLogic
     Public Function GetDefaultGLFormat() As GLFormat Implements IGLAble.GetDefaultGLFormat
       ''Hack เอา Bug #859 เมนูปรับปรุงสถานะเช็คจ่าย หากเป็นเช็คยกเลิก การบันทึกบัญชีไม่ลงตามการเชื่อมบัญชี 
       '' เพราะ เป็นเอกสารที่มีสอง autorun อยู่แล้ว ถ้าต้องแก้จริงๆ คือ 1 Autorun ให้มีผลต่อ สถานะเช็คไปด้วยเลยก็จะยากขึ้น
-      If Me.UpdatedStatus.Value <> 0 Then
-        If Not Me.AutoCodeFormat.GLFormat Is Nothing AndAlso Me.AutoCodeFormat.GLFormat.Originated Then
-          Return Me.AutoCodeFormat.GLFormat
-        End If
+      'If Me.UpdatedStatus.Value <> 0 Then
+      If Not Me.AutoCodeFormat.GLFormat Is Nothing AndAlso Me.AutoCodeFormat.GLFormat.Originated Then
+        Return Me.AutoCodeFormat.GLFormat
       End If
+      'End If
 
       Dim ds As DataSet = SqlHelper.ExecuteDataset(Me.ConnectionString _
                   , CommandType.StoredProcedure _
