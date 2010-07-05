02fe4c936b8576edbaa62829a4a09945106f57ec
KRISS <KRISS@a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a>
2010-07-05 04:51:34 +0000

    [Bug#1023] Revert 604 ครับผม
    git-svn-id: https://opserver:443/svn/pojjaman/trunk@788 a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a

--- AdobeForm/DesignerForm.vb
+++ AdobeForm/DesignerForm.vb
@@ -1624,10 +1624,15 @@ Namespace Longkong.AdobeForm
 
 
                       Dim dataFont As Font
-                      If Not _Font Is Nothing Then
+                      If Not col.Font Is Nothing Then
+                        If Not _Font Is Nothing Then
+                          'ใช้ Style จาก font ใน item
+                          dataFont = New Font(col.Font.FontFamily.Name, col.Font.Size, _Font.Style, GraphicsUnit.Point, CType(222, Byte))
+                        Else
+                          dataFont = New Font(col.Font.FontFamily.Name, col.Font.Size, col.Font.Style, GraphicsUnit.Point, CType(222, Byte))
+                        End If
+                      ElseIf Not _Font Is Nothing Then
                         dataFont = New Font(_Font.FontFamily.Name, _Font.Size, _Font.Style, GraphicsUnit.Point, CType(222, Byte))
-                      ElseIf Not col.Font Is Nothing Then
-                        dataFont = New Font(col.Font.FontFamily.Name, col.Font.Size, col.Font.Style, GraphicsUnit.Point, CType(222, Byte))
                       Else
                         dataFont = New Font(tb.Font.FontFamily.Name, tb.Font.Size, tb.Font.Style, System.Drawing.GraphicsUnit.Point, tb.Font.GdiCharSet)
                       End If
