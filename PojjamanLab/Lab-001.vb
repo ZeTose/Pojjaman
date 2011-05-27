e925fdba78a785a9076fc65085ff48cf3d93dc91
chef <chef@a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a>
2011-05-27 02:53:40 +0000

    git-svn-id: https://opserver:443/svn/pojjaman/trunk@1574 a7d61aa6-360a-e74d-bbce-eeb7ac27cc1a

---
+++ PojjamanLab/Lab-001.vb
@@ -0,0 +1,3 @@
+﻿Public Class Lab_001
+
+End Class
---
+++ PojjamanLab/MainLab.vb
@@ -0,0 +1,9 @@
+﻿Public Class MainLab
+
+    Private Sub btnLab001_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLab001.Click
+        Dim frm As New Lab_001
+
+        frm.Show()
+
+    End Sub
+End Class
