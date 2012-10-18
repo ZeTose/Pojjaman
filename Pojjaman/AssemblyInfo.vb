f98466594006ef4fcc0535b5a5bc74cd83995d4a
pui <julawut@longkongstudio.com>
2012-10-18 17:30:14 +0700

    ลืม update เลขเวอร์ชัน

--- Pojjaman/AssemblyInfo.vb
+++ Pojjaman/AssemblyInfo.vb
@@ -29,4 +29,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*'
 
-<Assembly: AssemblyVersion("1.28.0002.*")> 
+<Assembly: AssemblyVersion("1.28.0003.*")> 
