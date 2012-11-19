424322daddd37dbe238c0655eddd32f6e6dcd68e
pui <julawut@longkongstudio.com>
2012-11-19 15:49:47 +0700

    closed #255 เพิ่มประสิทธิภาพ การบันทึกเอกสาร หน้าโอนย้ายวัสดุ ให้เร็วขึ้น

--- Pojjaman.BusinessLogic/BusinessEntities/MaterialWitdrawal/MatOperationWithdrawItem.vb
+++ Pojjaman.BusinessLogic/BusinessEntities/MaterialWitdrawal/MatOperationWithdrawItem.vb
@@ -225,6 +225,7 @@ Namespace Longkong.Pojjaman.BusinessLogic
                 , New SqlParameter("@EntityId", 31) _
                 , New SqlParameter("@lci_id", lci_id) _
                 , New SqlParameter("@stock_id", Me.MatOperationWithdraw.Id) _
+                , New SqlParameter("@itemcheck_byitem", True) _
                 )
         If ds.Tables(0).Rows(0).IsNull("remain") Then
           Return 0
